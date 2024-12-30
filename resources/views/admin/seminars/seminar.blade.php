dab8c8bde9439fd58bed69ad82748742291cea2a
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-12-30 12:18:08 +0800

    refactor seminar layout: enhance header structure, improve table styling, and format date display

--- resources/views/admin/seminars/seminar.blade.php
+++ resources/views/admin/seminars/seminar.blade.php
@@ -1,28 +1,39 @@
 <x-app-layout>
-    <h1>Seminar</h1>
+    <div class="content">
+        <div class="container-fluid">
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th></th>
-                <th>Title</th>
-                <th>Desription</th>
-                <th>Location</th>
-                <th>Date</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($seminar as $index => $data)
-                <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $data->title }}</td>
-                    <td>{{ $data->description }}</td>
-                    <td>{{ $data->location }}</td>
-                    <td>{{ $data->date }}</td>
-                    <td></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <div class="page-header">
+                <div class="container-fluid">
+                    <h2 class="h5 no-margin-bottom">Seminar</h2>
+                    <hr class="mt-0">
+                </div>
+            </div>
+
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Title</th>
+                        <th>Desription</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                        {{-- <th>Action</th> --}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($seminar as $index => $data)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $data->title }}</td>
+                            <td>{{ $data->description }}</td>
+                            <td>{{ $data->location }}</td>
+                            <td>{{ \Carbon\Carbon::parse($data->date)->format('F d, Y') }}</td>
+                            {{-- <td></td> --}}
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+    </div>
 </x-app-layout>
